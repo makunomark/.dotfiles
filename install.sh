d1f60b62880a2da31742a669a0520bb4782d0db2
Mark Makuno <makunomark@gmail.com>
2024-05-09 11:23:44 +0200

    install nvm and omz plugins

--- install.sh
+++ install.sh
@@ -3,7 +3,19 @@ if [ -z "$DOTFILES_PATH" ]; then
   exit 1
 fi
 
-# Vim configuration
+# SymLink Vim configuration
 rm ~/.vimrc
 ln -sv "$DOTFILES_PATH/vim/vimrc" ~/.vimrc
+
+# Install omz plugins
+echo "Oh-my-zsh Plugins: Installing omz plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+echo "Oh-my-zsh Plugins: Install complete"
+
+# Install nvm
+echo "Node version manager: Installing..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+echo "Node version manager: Install complete"
 
